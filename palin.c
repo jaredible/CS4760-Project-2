4b0c5b804b5979149972493d0ab23832664a7196
Jared Diehl <diehl@hoare7.cs.umsl.edu>
2020-09-19 13:55:26 -0500

    palin.c

--- palin.c
+++ palin.c
@@ -1,5 +1,37 @@
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+bool is_palindrome(char *string);
 
 int main(int argc, char **argv) {
+//	char *strings[] = {"test", "radar", "Rotor", "aB0bA", "here"};
+//	int i;
+//	for (i = 0; i < 5; i++) {
+//		char *string = strings[i];
+//		printf("%s, %s\n", string, is_palindrome(string) ? "true" : "false");
+//	}
+	
 	return EXIT_SUCCESS;
+}
+
+bool is_palindrome(char *string) {
+	bool result = true;
+	
+	int index_left = 0, index_right = strlen(string) - 1;
+	char char_left, char_right;
+	
+	while (index_right > index_left) {
+		char_left = tolower(string[index_left]);
+		char_right = tolower(string[index_right]);
+		
+		if (char_left != char_right) return false;
+		
+		index_left++;
+		index_right--;
+	}
+	
+	return result;
 }
