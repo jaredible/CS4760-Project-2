c7acab15e620525127e3eec439f6a09fa095d9c4
Jared Diehl <diehl@hoare7.cs.umsl.edu>
2020-09-19 15:12:01 -0500

    Implemented checking for options

--- master.c
+++ master.c
@@ -1,5 +1,49 @@
+#include <getopt.h>
+#include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+const char *program_name = NULL;
+
+void set_program_name(const char *name);
+void usage(int status);
 
 int main(int argc, char **argv) {
-	return EXIT_SUCCESS;
+	set_program_name(argv[0]);
+	
+	bool ok = true;
+	
+	while (true) {
+		int c = getopt(argc, argv, "hn:s:t:");
+		
+		if (c == -1) break;
+		
+		switch (c) {
+			case 'h':
+				break;
+			case 'n':
+				break;
+			case 's':
+				break;
+			case 't':
+				break;
+			default:
+				ok = false;
+		}
+	}
+	
+	if (!ok) usage(EXIT_FAILURE);
+	
+	return ok ? EXIT_SUCCESS : EXIT_FAILURE;
+}
+
+void set_program_name(const char *name) {
+	program_name = name;
+}
+
+void usage(int status) {
+	if (status != EXIT_SUCCESS) fprintf(stderr, "Try '%s -h' for more information.\n", program_name);
+	else {
+	}
 }
