511ba326a32f5ee41d84b20cfa65e3198f80097d
Jared Diehl <diehl@hoare7.cs.umsl.edu>
2020-09-29 10:03:42 -0500

    Initial prototype

--- master.c
+++ master.c
@@ -1,28 +1,102 @@
 #include <ctype.h>
 #include <getopt.h>
+#include <signal.h>
 #include <stdarg.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <unistd.h>
 
-#define DEFAULT_TOTAL_CHILD_COUNT 4
-#define DEFAULT_CONCURRENT_CHILD_COUNT 2
-#define DEFAULT_TERMINATION_TIMEOUT 100
+#define LINE_COUNT 100
+#define STRING_LENGTH 256
 
-const char *program_name = NULL;
+struct strings {
+	char data[LINE_COUNT][STRING_LENGTH];
+};
 
-void set_program_name(const char *name);
-void error(const char *fmt, ...);
-void usage(int status);
+void clearLogFiles();
+void removeNewline(char*);
+int loadStrings(char*);
+void spawnChild(int);
+void spawn(int);
+void executeChild(int, int);
+void killSignalHandler(int);
+void timerSignalHandler(int);
+void releaseMemory();
+
+const int MAX_NUM_OF_PROCESSES_IN_SYSTEM = 20;
+int currentConcurrentChildCount = 0; // FIXME
+
+int stringsKey;
+int stringsSegmentID;
+struct strings* strings;
+
+int childrenKey;
+int childrenSegmentID;
+int* children;
+
+int flagsKey;
+int flagsSegmentID;
+int *flags;
+
+int turnKey;
+int turnSegmentID;
+int *turn;
+
+int childProcessGroupKey;
+int childProcessGroupSegmentID;
+pid_t* childProcessGroup;
+
+int status = 0;
+
+int startTime;
+
+int maximumChildCount = 4;
+int concurrentChildCount = 2;
+int executedChildCount = 0;
+int durationBeforeTermination = 5;
+
+void setupTimer(int timeout) {
+	struct sigaction action;
+	memset(&action, 0, sizeof(action));
+	action.sa_handler = killSignalHandler;
+	if (sigaction(SIGALRM, &action, NULL) != 0) {
+		perror("sigaction");
+		abort();
+	}
+	
+	struct itimerval timer;
+	timer.it_value.tv_sec = timeout / 1000;
+	timer.it_value.tv_usec = (timeout % 1000) * 1000;
+	
+	timer.it_interval.tv_sec = 0;
+	timer.it_interval.tv_usec = 0;
+	
+	if (setitimer(ITIMER_REAL, &timer, NULL) != 0) {
+		perror("setitimer");
+		abort();
+	}
+}
 
 int main(int argc, char **argv) {
-	set_program_name(argv[0]);
+	setvbuf(stdout, NULL, _IONBF, 0);
 	
-	bool ok = true;
-	int total_child_count = DEFAULT_TOTAL_CHILD_COUNT;
-	int concurrent_child_count = DEFAULT_CONCURRENT_CHILD_COUNT;
-	int termination_timeout = DEFAULT_TERMINATION_TIMEOUT;
+	setupTimer(10 * 1000);
+	signal(SIGINT, killSignalHandler);
+//	signal(SIGUSR2, timerSignalHandler);
+	
+	stringsKey = ftok("Makefile", 1);
+	childrenKey = ftok("Makefile", 2);
+	flagsKey = ftok("Makefile", 3);
+	turnKey = ftok("Makefile", 4);
+	childProcessGroupKey = ftok("Makefile", 5);
+	
+	clearLogFiles();
 	
 	while (true) {
 		int c = getopt(argc, argv, "hn:s:t:");
@@ -31,93 +105,222 @@ int main(int argc, char **argv) {
 		
 		switch (c) {
 			case 'h':
-				usage(EXIT_SUCCESS);
+				printf("NAME\n");
+				printf("       %s - palindrome finder");
+				printf("\nUSAGE\n");
+				printf("       %s [-h]\n", argv[0]);
+				printf("       %s [-n x] [-s x] [-t time] infile", argv[0]);
+				printf("\nDESCRIPTION\n");
+				printf("       -h       : Print a help message or usage, and exit\n");
+				printf("       -n x     : Maximum total of child processes\n");
+				printf("       -s x     : Number of children allowed to exist concurrently\n");
+				printf("       -t time  : Time, in seconds, after which the program will terminate\n");
+				exit(0);
 			case 'n':
-				if (isdigit(*optarg)) {
-					total_child_count = atoi(optarg);
-				} else {
-					error("invalid total child count '%s'", optarg);
-					ok = false;
+				maximumChildCount = atoi(optarg);
+				if (maximumChildCount < 0) {
+					fprintf(stderr, "Negative arguments are not valid\n");
+					exit(1);
 				}
 				break;
 			case 's':
-				if (isdigit(*optarg)) {
-					concurrent_child_count = atoi(optarg);
-				} else {
-					error("invalid concurrent child count '%s'", optarg);
-					ok = false;
+				concurrentChildCount = atoi(optarg);
+				if (concurrentChildCount < 0) {
+					fprintf(stderr, "Cannot spawn a negative number of children\n");
+					exit(1);
 				}
 				break;
 			case 't':
-				if (isdigit(*optarg)) {
-					termination_timeout = atoi(optarg);
-				} else {
-					error("invalid termination timeout '%s'", optarg);
-					ok = false;
+				durationBeforeTermination = atoi(optarg);
+				if (durationBeforeTermination < 0) {
+					fprintf(stderr, "Master cannot have a run duration of negative time\n");
+					exit(1);
 				}
 				break;
 			default:
-				ok = false;
+				fprintf(stderr, "Default getopt statement\n");
+				exit(1);
 		}
 	}
 	
-	if (argv[optind] == NULL || argc > optind + 1) {
-		error("invalid arguments specified");
-		ok = false;
+	int stringCount;
+	
+	if ((stringsSegmentID = shmget(stringsKey, sizeof(struct strings), IPC_CREAT | S_IRUSR | S_IWUSR)) < 0) {
+		perror("shmget: Failed to allocate shared memory for string array");
+		exit(1);
+	} else {
+		strings = (struct strings*) shmat(stringsSegmentID, NULL, 0);
+		stringCount = loadStrings(argv[optind]);
+		if (maximumChildCount != stringCount) maximumChildCount = stringCount;
 	}
 	
-	if (!ok) usage(EXIT_FAILURE);
-	
-	// input file specified
-	
-	return ok ? EXIT_SUCCESS : EXIT_FAILURE;
-}
-
-void set_program_name(const char *name) {
-	program_name = name;
-}
-
-void error(const char *fmt, ...) {
-	int n;
-	int size = 100;
-	char *p, *np;
-	va_list ap;
-	
-	if ((p = malloc(size)) == NULL) return;
-	
-	while (true) {
-		va_start(ap, fmt);
-		n = vsnprintf(p, size, fmt, ap);
-		va_end(ap);
-		
-		if (n < 0) return;
-		
-		if (n < size) break;
-		
-		size = n + 1;
-		
-		if ((np = realloc(p, size)) == NULL) {
-			free(p);
-			return;
-		} else p = np;
+	if ((childrenSegmentID = shmget(childrenKey, sizeof(int), IPC_CREAT | S_IRUSR | S_IWUSR)) < 0) {
+		perror("shmget: Failed to allocate shared memory for child count");
+		exit(1);
+	} else {
+		children = (int*) shmat(childrenSegmentID, NULL, 0);
+		*children = stringCount;
 	}
 	
-	fprintf(stderr, "%s: %s\n", program_name, p);
+	if ((flagsSegmentID = shmget(flagsKey, *children * sizeof(int), IPC_CREAT | S_IRUSR | S_IWUSR)) < 0) {
+		perror("shmget: Failed to allocate shared memory for flag array");
+		exit(1);
+	} else {
+		flags = (int*) shmat(flagsSegmentID, NULL, 0);
+	}
+	
+	if ((turnSegmentID = shmget(turnKey, sizeof(int), IPC_CREAT | S_IRUSR | S_IWUSR)) < 0) {
+		perror("shmget: Failed to allocate shared memory for turn");
+		exit(1);
+	} else {
+		turn = (int*) shmat(turnSegmentID, NULL, 0);
+	}
+	
+	if ((childProcessGroupSegmentID = shmget(childProcessGroupKey, sizeof(pid_t), IPC_CREAT | S_IRUSR | S_IWUSR)) < 0) {
+		perror("shmget: Failed to allocate shared memory for group PID");
+		exit(1);
+	} else {
+		childProcessGroup = (pid_t*) shmat(childProcessGroupSegmentID, NULL, 0);
+	}
+	
+	startTime = time(0);
+	
+	int childIndex = 0;
+	
+	while (childIndex < concurrentChildCount)
+		spawnChild(childIndex++);
+
+	while(currentConcurrentChildCount > 0) {
+		wait(NULL);
+		currentConcurrentChildCount--;
+		printf("[finish] %d processes in system\n", currentConcurrentChildCount);
+		spawnChild(childIndex++);
+	}
+	
+	releaseMemory();
+	
+	return 0;
 }
 
-void usage(int status) {
-	if (status != EXIT_SUCCESS) fprintf(stderr, "Try '%s -h' for more information.\n", program_name);
-	else {
-		printf("NAME\n");
-		printf("       %s - palindrome finder");
-		printf("\nUSAGE\n");
-		printf("       %s [-h]\n", program_name);
-		printf("       %s [-n x] [-s x] [-t time] infile", program_name);
-		printf("\nDESCRIPTION\n");
-		printf("       -h       : Print a help message or usage, and exit\n");
-		printf("       -n x     : Maximum total of child processes\n");
-		printf("       -s x     : Number of children allowed to exist concurrently\n");
-		printf("       -t time  : Time, in seconds, after which the program will terminate\n");
+void clearLogFiles() {
+	char* files[] = {"palin.out", "nopalin.out", "output.log"};
+	int i = 0;
+	for (; i < 3; i++) {
+		FILE* fp = fopen(files[i], "w");
+		fclose(fp);
 	}
-	exit(status);
+}
+
+void removeNewline(char* s) {
+	while (*s) {
+		if (*s == '\n') *s = '\0';
+		s++;
+	}
+}
+
+int loadStrings(char* path) {
+	FILE* fp = fopen(path, "r");
+	if (fp == NULL) {
+		perror("fopen: Failed to open file for loading strings");
+		exit(1);
+	}
+	
+	int i = 0;
+	char* line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	while ((read = getline(&line, &len, fp)) != -1) {
+		removeNewline(line);
+		strcpy(strings->data[i++], line);
+	}
+	
+	fclose(fp);
+	if (line) free(line);
+	
+	return i;
+}
+
+void spawnChild(int childIndex) {
+	bool canSpawnChild = currentConcurrentChildCount < concurrentChildCount && executedChildCount < maximumChildCount && executedChildCount < MAX_NUM_OF_PROCESSES_IN_SYSTEM;
+	if (canSpawnChild) {
+		spawn(childIndex);
+	} else {
+//		printf("HERE %d\n", childIndex);
+//		waitpid(-(*childProcessGroup), &status, 0);
+//		currentConcurrentChildCount--;
+//		printf("[idk] %d processes in system.\n", currentConcurrentChildCount);
+//		spawn(childIndex);
+	}
+	executedChildCount++;
+}
+
+void spawn(int childIndex) {
+	currentConcurrentChildCount++;
+	if (fork() == 0) {
+		printf("[start] %d processes in system.\n", currentConcurrentChildCount);
+		if (childIndex == 0) *childProcessGroup = getpid();
+		setpgid(0, *childProcessGroup);
+		printf("[spawn] ppid: %d, pid: %d, childProcessGroup: %d\n", getppid(), getpid(), *childProcessGroup);
+		executeChild(childIndex, childIndex);
+		exit(0);
+	}
+}
+
+void executeChild(int childIndex, int stringIndex) {
+	char ci[256];
+	sprintf(ci, "%d", childIndex);
+	
+	char si[256];
+	sprintf(si, "%d", stringIndex);
+	
+	execl("./palin", "palin", ci, si, (char*) NULL);
+}
+
+void killSignalHandler(int signal) {
+	printf("master: Exiting due to interrupt signal\n");
+	
+	killpg(*childProcessGroup, SIGTERM);
+	
+	int i = 0;
+	for (; i < currentConcurrentChildCount; i++)
+		wait(NULL);
+	
+	releaseMemory();
+	
+	exit(0);
+}
+
+void timerSignalHandler(int signal) {
+	if (time(0) - startTime >= durationBeforeTermination) {
+		printf("master: Timed out\n");
+		
+		killpg(*childProcessGroup, SIGUSR1);
+		
+		int i = 0;
+		for (; i < currentConcurrentChildCount; i++)
+			wait(NULL);
+		
+		releaseMemory();
+		
+		exit(0);
+	}
+}
+
+void releaseMemory() {
+	printf("master: Releasing shared memory\n");
+	
+	shmdt(strings);
+	shmctl(stringsSegmentID, IPC_RMID, NULL);
+	
+	shmdt(children);
+	shmctl(childrenSegmentID, IPC_RMID, NULL);
+	
+	shmdt(flags);
+	shmctl(flagsSegmentID, IPC_RMID, NULL);
+	
+	shmdt(turn);
+	shmctl(turnSegmentID, IPC_RMID, NULL);
+	
+	shmdt(childProcessGroup);
+	shmctl(childProcessGroupSegmentID, IPC_RMID, NULL);
 }
--- palin.c
+++ palin.c
@@ -1,30 +1,204 @@
 #include <ctype.h>
+#include <signal.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <unistd.h>
 
-bool is_palindrome(char *string);
+#define LINE_COUNT 100
+#define STRING_LENGTH 256
 
-int main(int argc, char **argv) {
-	return EXIT_SUCCESS;
-}
+enum state { idle, want_in, in_cs };
 
-bool is_palindrome(char *string) {
-	bool result = true;
+struct strings {
+	char data[LINE_COUNT][STRING_LENGTH];
+};
+
+bool isPalindrome(char*);
+void logPalin(bool, char*);
+void logChild(int, int, char*);
+void terminateSignalHandler(int);
+void timeoutSignalHandler(int);
+char* getFormattedTime();
+
+int i;
+
+int main(int argc, char** argv) {
+	setvbuf(stdout, NULL, _IONBF, 0);
 	
-	int index_left = 0, index_right = strlen(string) - 1;
-	char char_left, char_right;
+	signal(SIGTERM, terminateSignalHandler);
+//	signal(SIGUSR1, timeoutSignalHandler);
 	
-	while (index_right > index_left) {
-		char_left = tolower(string[index_left]);
-		char_right = tolower(string[index_right]);
-		
-		if (char_left != char_right) return false;
-		
-		index_left++;
-		index_right--;
+	int index;
+	
+	if (argc < 2) {
+		printf("No arguments supplied");
+		exit(1);
+	} else {
+		i = atoi(argv[1]);
+		index = atoi(argv[2]);
 	}
 	
+	printf("index: %d, ppid: %d, pid: %d\n", i, getppid(), getpid());
+	
+	srand(time(NULL) + i);
+	
+	int N;
+	
+	int stringsKey = ftok("Makefile", 1);
+	int stringsSegmentID;
+	struct strings* strings;
+	
+	if ((stringsSegmentID = shmget(stringsKey, sizeof(struct strings), IPC_CREAT | S_IRUSR | S_IWUSR)) < 0) {
+		perror("shmget: Failed to allocate shared memory for string array");
+		exit(1);
+	} else {
+		strings = (struct strings*) shmat(stringsSegmentID, NULL, 0);
+	}
+	
+	int childrenKey = ftok("Makefile", 2);
+	int childrenSegmentID;
+	int* children;
+	
+	if ((childrenSegmentID = shmget(childrenKey, sizeof(int), IPC_CREAT | S_IRUSR | S_IWUSR)) < 0) {
+		perror("shmget: Failed to allocate to shared memory for child count");
+		exit(1);
+	} else {
+		children = (int*) shmat(childrenSegmentID, NULL, 0);
+		N = *children;
+	}
+	
+	int flagsKey = ftok("Makefile", 3);
+	int flagsSegmentID;
+	int* flags;
+	
+	if ((flagsSegmentID = shmget(flagsKey, N * sizeof(int), IPC_CREAT | S_IRUSR | S_IWUSR)) < 0) {
+		perror("shmget: Failed to allocate shared memory for flag array");
+		exit(1);
+	} else {
+		flags = (int*) shmat(flagsSegmentID, NULL, 0);
+	}
+	
+	int turnKey = ftok("Makefile", 4);
+	int turnSegmentID;
+	int* turn;
+	
+	if ((turnSegmentID = shmget(turnKey, sizeof(int), IPC_CREAT | S_IRUSR | S_IWUSR)) < 0) {
+		perror("shmget: Failed to allocate shared memory for turn");
+		exit(1);
+	} else {
+		turn = (int*) shmat(turnSegmentID, NULL, 0);
+	}
+	
+	char* string = strings->data[index];
+	bool palindrome = isPalindrome(string);
+//	fprintf(stderr, "%s: Process %d determined that %s is %s\n", getFormattedTime(), i, string, palindrome ? "a palindrome" : "not a palindrome");
+	
+	fprintf(stderr, "%s: Process %d wants to enter critical section\n", getFormattedTime(), i);
+	
+	int j;
+	do {
+		flags[i] = want_in;
+		j = *turn;
+		
+		while (j != i)
+			j = (flags[j] != idle) ? *turn : (j + 1) % N;
+		
+		flags[i] = in_cs;
+		
+		for (j = 0; j < N; j++)
+			if (j != i && flags[j] == in_cs)
+				break;
+	} while (j < N || (*turn != i && flags[*turn] != idle));
+	
+	*turn = i;
+	
+	/* Enter critical section */
+	
+	fprintf(stderr, "%s: Process %d in critical section\n", getFormattedTime(), i);
+	sleep(rand() % 3);
+	logPalin(palindrome, string);
+	logChild(getpid(), i, string);
+	fprintf(stderr, "%s: Process %d exiting critical section\n", getFormattedTime(), i);
+	
+	/* Exit critical section */
+	
+	j = (*turn + 1) % N;
+	while (flags[j] == idle)
+		j = (j + 1) % N;
+	
+	*turn = j;
+	flags[i] = idle;
+	
+	/* Enter remainder section */
+	
+//	kill(getppid(), SIGUSR2);
+	
+	/* Exit remainder section */
+	
+	return 0;
+}
+
+bool isPalindrome(char* string) {
+	int leftIndex = 0, rightIndex = strlen(string) - 1;
+	char leftChar, rightChar;
+	
+	while (rightIndex > leftIndex) {
+		leftChar = tolower(string[leftIndex]);
+		rightChar = tolower(string[rightIndex]);
+		
+		if (leftChar != rightChar) return false;
+		
+		leftIndex++;
+		rightIndex--;
+	}
+	
+	return true;
+}
+
+void logPalin(bool palindrome, char* string) {
+	FILE* fp = fopen(palindrome ? "palin.out" : "nopalin.out", "a+");
+	if (fp == NULL) {
+		perror("fopen: Failed to open file for palin output");
+		exit(1);
+	}
+	fprintf(fp, "%s\n", string);
+	fclose(fp);
+}
+
+void logChild(int pid, int index, char* string) {
+	FILE* fp = fopen("output.log", "a+");
+	if (fp == NULL) {
+		perror("fopen: Failed to open file for child output");
+		exit(1);
+	}
+	fprintf(fp, "%d %d %s\n", pid, index, string);
+	fclose(fp);
+}
+
+void terminateSignalHandler(int signal) {
+	if (signal == SIGTERM) {
+		printf("palin: Process %d exiting due to interrupt signal\n", i);
+		exit(1);
+	}
+}
+
+void timeoutSignalHandler(int signal) {
+	if (signal == SIGUSR1) {
+		printf("palin: Process %d exiting due to timeout\n", i);
+		exit(1);
+	}
+}
+
+char* getFormattedTime() {
+	int n = 100;
+	char* result = malloc(n * sizeof(char));
+	time_t now = time(0);
+	strftime(result, n, "%H:%M:%S", localtime(&now));
 	return result;
 }
